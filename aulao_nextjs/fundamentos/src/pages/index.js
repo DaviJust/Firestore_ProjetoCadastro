8cbcdd0287e2ea4976be136957c117e37c8004e2
Davi <davijustinocouto@gmail.com>
2023-01-06 18:06:01 -0300

    Exercicios

--- aulao_nextjs/fundamentos/src/pages/index.js
+++ aulao_nextjs/fundamentos/src/pages/index.js
@@ -2,11 +2,30 @@
 //   //"<h1>Titulo</h1>" vai ler como string
 //   return <h1>Titulo</h1>
 // }
-import Estiloso from "./Estiloso"
-export default function framework(){
+// import Estiloso from "./estiloso"
+// export default function framework(){
+//   return (
+//     <div>
+//       <link href="/Estiloso">
+//       Estiloso
+//       </link>
+//     </div>
+//   )
+// }
+
+import Navegador from "../components/Navegador"
+
+export default function Inicio() {
   return (
-    <div>
-    <Estiloso></Estiloso>
+    <div style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+
+    }}>
+      <Navegador destino="/estiloso" texto="estiloso " />
+      <Navegador destino="/exemplo" texto="exemplo "/>
+      <Navegador destino="/jsx" texto="jsx "/>
     </div>
   )
 }
